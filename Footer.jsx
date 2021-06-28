74013e943815255f2346abe1cefb505938bf4f86
omsingh32123 <86588227+omsingh32123@users.noreply.github.com>
2021-06-28 11:22:17 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function Footer() {
+  return (
+    <Footer>
+      <p>Copyright By VAOM cooperation @ {new Date().getFullYear()}</p>
+    </Footer>
+  );
+}
+
+export default Footer;
---
+++ Info.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+function Info() {
+  return (
+    <div class="note">
+      <h1>INDIAN GOVERMENT</h1>
+      <h1>Subject To Promote Indian History</h1>
+      <h3>By</h3>
+      <h3>Pm Narendra Modi</h3>
+      <h3>Cm Yogi Adityanath</h3>
+
+      <p>
+        According to consensus in modern genetics anatomically modern humans
+        first arrived on the Indian subcontinent from Africa between 73,000 and
+        55,000 years ago. However, the earliest known human remains in South
+        Asia date to 30,000 years ago. Settled life, which involves the
+        transition from foraging to farming and pastoralism, began in South Asia
+        around 7,000 BCE.
+      </p>
+    </div>
+  );
+}
+
+export default Info;
